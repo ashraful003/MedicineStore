931f23fb0ed123315829cbf967063851e959414e
ashraful003 <ashraful10178@gmail.com>
2024-04-01 02:17:09 +0800

    Designed Fragment Medicine List AA MS-1012

---
+++ app/src/main/java/com/example/medicinestore/presentation/dashboard/medicin/Medicine.kt
@@ -0,0 +1,13 @@
+package com.example.medicinestore.presentation.dashboard.medicin
+
+data class Medicine(
+    var medicineId:String? = null,
+    var name:String? = null,
+    var company:String? = null,
+    var price:String? = null,
+    var expireDate:String? = null,
+    var details:String? = null,
+    var self:String? = null,
+    var row:String? = null,
+    var column:String? = null
+)
