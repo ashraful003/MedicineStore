2c20a4819ffe821c66fe87cd0cc3bbe0a1821b26
ashraful003 <ashraful10178@gmail.com>
2024-04-07 03:56:23 +0800

    Developed Add Medicine Fragment AA MS-1022

--- app/src/main/java/com/example/medicinestore/presentation/dashboard/medicin/Medicine.kt
+++ app/src/main/java/com/example/medicinestore/presentation/dashboard/medicin/Medicine.kt
@@ -4,9 +4,9 @@ data class Medicine(
     var medicineId:String? = null,
     var name:String? = null,
     var company:String? = null,
+    var details:String? = null,
     var price:String? = null,
     var expireDate:String? = null,
-    var details:String? = null,
     var self:String? = null,
     var row:String? = null,
     var column:String? = null
